ba282ed08c3aa51c05556f154c35f9dd3fc52f7c
Ben <85500722+Ben-Bussch@users.noreply.github.com>
2025-10-16 09:44:46 +0100

    Added some debugging in setup through rs485

--- RS_MASTER/RS_MASTER/src/main.cpp
+++ RS_MASTER/RS_MASTER/src/main.cpp
@@ -13,6 +13,8 @@ File logFile;
 char logFileName[32];
 int SD_pin = 4;
 
+int clk_time = 0;
+
 // Fire/Fill sequence variables
 int PyroPin = 21;
 int FillSequPin = 36;
@@ -46,11 +48,9 @@ void sendString(const String& data) {
     //Serial.println(data);
 }
 
-int clk_time = 0;
 
-
-String firingpinstatus = "";
-String fillpinstatus = "";
+//String firingpinstatus = "";
+//String fillpinstatus = "";
 
 //testing :)
 //--------------- My good'ol rs485 -------------------------
@@ -67,13 +67,19 @@ void setup() {
     //Serial.println("Hello from Teensy 4.1!");
     while (!Serial2 && millis() < 5000) {}
 
-    //Serial2.flush();
-    String PT_setup = SetupCurrentSensor();
-    
-    //Serial.println(PT_setup);
-
     pinMode(RS_DE_RE_SLAVE, OUTPUT);
     digitalWrite(RS_DE_RE_SLAVE, LOW);   // default receive
+    sendString(String("RS485 Slave ready."));
+
+    //Serial2.flush();
+    String PT_setup = SetupCurrentSensor();
+    sendString(PT_setup);
+    //Serial.println(PT_setup);
+
+    //Inputs and Outputs
+    SetupControl(PyroPin, FirePin, FillSequPin);
+    
+
 
 /* 
     if (!SD.begin(SD_pin)) {
@@ -91,7 +97,7 @@ void setup() {
   
 }*/
     //Serial.println("RS485 Slave ready.");
-    SetupControl(PyroPin, FirePin, FillSequPin);
+
 }
 
 void loop() {
